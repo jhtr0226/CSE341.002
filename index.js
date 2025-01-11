062a45ea0e80a4acaa600df1328de6e7e29d0843
jhtr0226 <johanncraft17@gmail.com>
2025-01-11 14:35:07 -0600

    index.js added

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 require('dotenv').config(); // Load .env variables
 
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
+const router = express.Router();
+
+
+
+const app = express();
 const mongoUrl = process.env.MONGODB_URL
-
-
-
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || 'localhost';
 
 
-mongoose.connect(mongoUrl, { useNewUrlParse: true, useUnifiedTopology: true })
+//Middleware
+app.use(express.json());
+
+//MongoDB Connection
+mongoose.connect(mongoUrl)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
 
-
+//Routes
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
+router.get('/home', (req, res) => {
+  res.send('Welcome!')
+});
+
+
+
+
+app.use('/', router);
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://${host}:${port}`);
